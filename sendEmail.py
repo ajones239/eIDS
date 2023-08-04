4765031c0ada6370a4c3bc1b2b3ec1f3d197bc3b
ArpithaSrinivas <arpitha.srinivas@sjsu.edu>
2023-08-04 15:07:02 -0700

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -1,1 +1,37 @@
+import smtplib
+import ssl
+import time
+
+# Function to check for attacks using the ML model
+def detect_attack(data):
+    # Your ML model code here
+    # Returns True if attack is detected, False otherwise
+    return False
+
+# Function to send an email
+def send_email(subject, body):
+    sender_email = "your_sender_email@gmail.com"
+    receiver_emails = ["group_member1@example.com", "group_member2@example.com"]
+    password = "your_sender_email_password"
+
+    message = f"Subject: {subject}\n\n{body}"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_emails, message)
+
+# Main loop to check for attacks and send email alerts
+def main():
+    while True:
+        data = # Get relevant data for ML model
+        if detect_attack(data):
+            subject = "Alert: Potential Attack Detected"
+            body = "Our ML model detected a potential attack in the system. Please take necessary actions."
+            send_email(subject, body)
+        time.sleep(300)  # Wait for 5 minutes before checking again
+
+if __name__ == "__main__":
+    main()
 
