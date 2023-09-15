4d37acba9a833ae93329b58a4598967c34082fc9
Aidan Jones <arjones239@protonmail.ch>
2023-09-14 17:48:42 -0700

    debug msgs

--- modules/csv-importer.py
+++ modules/csv-importer.py
@@ -1,12 +1,11 @@
-from modules import *
 import numpy
 
-class CSVImporter(InputParsingModule):
+class CSVImporter(modules.Module, modules.IOModule):
 
     def __init__(self):
         super(CSVImporter, self).__init__()
         self.stream = False
-        self.dataFrequency = DataFrequency.ONE_TIME_ACCESS
+        self.dataFrequency = modules.DataFrequency.ONE_TIME_ACCESS
 
     def getOutput(self):
         return self.output
